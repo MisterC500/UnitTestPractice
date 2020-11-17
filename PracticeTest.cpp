371046d7757626c1a9ca98a7d0e965fef8b76cdc
MisterC500 <misterc500@gmail.com>
2020-11-16 22:47:52 -0800

    Added 3 test suites, changed code in DESC order

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -20,3 +20,32 @@ TEST(PracticeTest, is_simple_palindrome)
     bool actual = obj.isPalindrome("Not a palindrome");
     ASSERT_TRUE(actual);
 }
+
+TEST(PracticeTest, is_pal)
+{
+  Practice obj;
+  bool actual = obj.isPalindrome("aba");
+  ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_simple_descending)
+{
+  Practice obj;
+  int first = 200;
+  int second = 50;
+  int third = 100;
+  obj.sortDescending(first, second,third);
+  ASSERT_GE(first,second);
+  ASSERT_GE(second, third);
+}
+
+TEST(PracticeTest, is_desc)
+{
+  Practice obj;
+  int first = 0;
+  int second = 2;
+  int third = 0;
+  bool actual = obj.sortDescending(first, second,third);
+  ASSERT_TRUE(actual);
+}
+
